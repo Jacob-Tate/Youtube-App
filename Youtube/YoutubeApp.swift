ef8e18764424a8007d9b43ae7c0cf7758be810c1
jacob-tate <jacob11163@gmail.com>
2021-01-05 19:39:24 -0800

    Initial Commit

---
+++ Youtube/YoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeApp.swift
+//  Youtube
+//
+//  Created by Jacob Tate on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
